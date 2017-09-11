4f7a8809d02aa00bc3bd68661d4ba297e061be4d
Mark D. Blackwell <markdblackwell01@gmail.com>
2017-09-11 19:44:57 -0400

    Improve append program

--- back-end/append.php
+++ back-end/append.php
@@ -1,36 +1,54 @@
 <?php
-// See https://stackoverflow.com/questions/24972424/php-create-or-write-append-in-text-file
-// Copyright (c) 2017 Mark D. Blackwell
-// Accept AJAX request to append to a comments file.
+/* Copyright (C) 2017 Mark D. Blackwell.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see http://www.gnu.org/licenses/ .
+
+See https://stackoverflow.com/questions/24972424/php-create-or-write-append-in-text-file
+
+Accept AJAX request to append to a comments file.
+*/
 
 // Constants:
-$comments_filename = "comments.txt";
-$ip_address = $_SERVER['REMOTE_ADDR'];
-$response_file_bad = array('response' => 'Unable to open oomments file!');
+$response_ok                    = array('response' => 'ok');
 $response_request_parameter_bad = array('response' => 'Unable to access POST parameter!');
-$response_ok = array('response' => 'ok');
+$response_file_bad              = array('response' => 'Unable to open oomments file!');
 
-// Depends upon whatever is above:
+$ip_address = $_SERVER['REMOTE_ADDR'];
+$comments_filename = "comments.txt";
+
+// Depends upon the above:
 
 // Code to run:
 header("content-type:application/json");
 
-// TODO: For security, use POST instead of GET.
-isset($_GET['comment']) or die(json_encode($response_request_parameter_bad));
-$comment = $_GET['comment'];
-
-isset($_GET['song']) or die(json_encode($response_request_parameter_bad));
-$song = $_GET['song'];
-
-isset($_GET['timestamp']) or die(json_encode($response_request_parameter_bad));
-$timestamp = $_GET['timestamp'];
-
 $myfile = fopen($comments_filename, "a") or die(json_encode($response_file_bad));
 
-$string_to_write_song = $ip_address . " " . $timestamp . " " . $song . "\n";
-fwrite($myfile, $string_to_write_song);
+// TODO: For security, change to use POST instead of GET.
+isset(       $_GET['timestamp']) or die(json_encode($response_request_parameter_bad));
+$timestamp = $_GET['timestamp'];
 
-$string_to_write_comment = $ip_address . " " . $timestamp . " " . $comment . "\n";
+isset       ($_GET['comment'  ]) or die(json_encode($response_request_parameter_bad));
+$comment   = $_GET['comment'  ];
+
+isset       ($_GET['song'     ]) or die(json_encode($response_request_parameter_bad));
+$song      = $_GET['song'     ];
+
+// Depends upon the above:
+
+$prefix = $timestamp . " " . $ip_address . " ";
+
+$string_to_write_comment = $prefix . $comment . "\n";
+$string_to_write_song    = $prefix . $song    . "\n";
+
+fwrite($myfile, $string_to_write_song);
 fwrite($myfile, $string_to_write_comment);
 
 fclose($myfile);
