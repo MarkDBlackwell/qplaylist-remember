a175377a4c9808d4be2ec3cb0531a4bca1475360
Mark D. Blackwell <markdblackwell01@gmail.com>
2017-09-11 17:04:09 -0400

    Add a test

--- tests/Example.elm
+++ tests/Example.elm
@@ -7,4 +7,16 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    todo "Implement our first test. See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!"
+    --Implement our first test.
+    --See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!
+    describe "some tests"
+        [ describe "some sub tests"
+            [ test "has no effect on a palindrome" <|
+                \_ ->
+                    let
+                        palindrome =
+                            "hannah"
+                    in
+                        Expect.equal palindrome (String.reverse palindrome)
+            ]
+        ]
