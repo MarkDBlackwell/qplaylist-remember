c92708886e94b336f8500f3ec99e902796674e35
Mark D. Blackwell <markdblackwell01@gmail.com>
2017-09-14 19:17:39 -0400

    Improve back-end PHP code

--- back-end/append.php
+++ back-end/append.php
@@ -16,43 +16,47 @@ See https://stackoverflow.com/questions/24972424/php-create-or-write-append-in-t
 Accept AJAX request to append to a comments file.
 */
 
-// Constants:
-$response_ok                    = array('response' => 'ok');
-$response_request_parameter_bad = array('response' => 'Unable to access POST parameter!');
-$response_file_bad              = array('response' => 'Unable to open oomments file!');
-
-$ip_address = $_SERVER['REMOTE_ADDR'];
-$comments_filename = "comments.txt";
-
-// Depends upon the above:
-
 // Code to run:
 header("content-type:application/json");
 
-$myfile = fopen($comments_filename, "a") or die(json_encode($response_file_bad));
+// Constants:
+$comments_filename = "comments.txt";
+$ip_address = $_SERVER['REMOTE_ADDR'];
+
+$response_file_bad_json              = json_encode(array('response' => 'Unable to open comments file!'));
+$response_ok_json                    = json_encode(array('response' => 'ok'));
+$response_request_parameter_bad_json = json_encode(array('response' => 'Unable to access POST parameter!'));
+
+// Depends upon the above:
+
+$myfile = fopen($comments_filename, "a") or die($response_file_bad_json);
 
 // TODO: For security, change to use POST instead of GET.
-isset(       $_GET['timestamp']) or die(json_encode($response_request_parameter_bad));
-$timestamp = $_GET['timestamp'];
-
-isset       ($_GET['comment'  ]) or die(json_encode($response_request_parameter_bad));
+isset       ($_GET['comment'  ]) or die($response_request_parameter_bad_json);
 $comment   = $_GET['comment'  ];
 
-isset       ($_GET['song'     ]) or die(json_encode($response_request_parameter_bad));
+isset       ($_GET['song'     ]) or die($response_request_parameter_bad_json);
 $song      = $_GET['song'     ];
+
+isset(       $_GET['timestamp']) or die($response_request_parameter_bad_json);
+$timestamp = $_GET['timestamp'];
 
 // Depends upon the above:
 
 $prefix = $timestamp . " " . $ip_address . " ";
 
+// Depends upon the above:
+
 $string_to_write_comment = $prefix . $comment . "\n";
 $string_to_write_song    = $prefix . $song    . "\n";
+
+// Depends upon the above:
 
 fwrite($myfile, $string_to_write_song);
 fwrite($myfile, $string_to_write_comment);
 
 fclose($myfile);
 
-echo json_encode($response_ok);
+echo $response_ok_json;
 exit();
 ?>
