ae4530cad78507e0687bed4cf9ef66e76d81c406
Mark D. Blackwell <markdblackwell01@gmail.com>
2017-09-17 15:13:19 -0400

    Correct Windows phone 8 JavaScript file

--- remember/RememberSongs-windows-phone-8-viewport-fix.js
+++ remember/RememberSongs-windows-phone-8-viewport-fix.js
@@ -1,9 +1,22 @@
-<!--
+/* Copyright (C) 2017 Mark D. Blackwell.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
    See http://trentwalton.com/2013/01/16/windows-phone-8-viewport-fix/
 
-   Keep as first script in <head>.
-   -->
-<script type="text/javascript">
+   Keep this as the first script in <head>.
+*/
+
 if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
 	var msViewportStyle = document.createElement('style');
 	msViewportStyle.appendChild(
@@ -14,4 +27,3 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
 	document.getElementsByTagName("head")[0].
 	appendChild(msViewportStyle);
 }
-</script>
