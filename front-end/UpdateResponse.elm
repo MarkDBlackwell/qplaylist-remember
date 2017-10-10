c1755ae5c6dbc0363f22e3bca7cf88003d73b19b
Mark D. Blackwell <markdblackwell01@gmail.com>
2017-10-10 12:02:36 -0400

    Refactor

--- front-end/UpdateResponse.elm
+++ front-end/UpdateResponse.elm
@@ -309,8 +309,11 @@ songsLatestResponseOk model httpResponseText =
                         |> Just
                 , awaitingServerResponse = awaitingServerResponseInit
               }
-            , Just alertMessageTextDecode
-                |> logAndFocusOld model ActionDecoding
+            , Cmd.batch
+                [ Just alertMessageTextDecode
+                    |> logAction ActionDecoding
+                , focusInputPossibly model
+                ]
             )
 
         Ok songsLatestNew ->
