0307c58af39952efd9f5bf61c3ff308d6dd9a108
Mark D. Blackwell <markdblackwell01@gmail.com>
2017-10-19 10:18:24 -0400

    Refactor

--- remember/SetUp.js
+++ remember/SetUp.js
@@ -29,7 +29,6 @@ functionResetSongsDevelopmentOnly();
 
 (function() {
     var functionDealWithElm;
-    var functionStorageIsAccessible;
 
     var keyStorage;
 
@@ -39,6 +38,7 @@ functionResetSongsDevelopmentOnly();
     functionDealWithElm = function() {
         var functionShowCommentButtons;
         var functionSongsRememberedRetrieved;
+        var functionStorageIsAccessible;
 
         var app;
         var node;
@@ -84,6 +84,70 @@ functionResetSongsDevelopmentOnly();
             songsRememberedRetrieved = JSON.parse(songsAsString);
             return songsRememberedRetrieved;
         }
+        functionStorageIsAccessible = function() {
+            //See: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+            var hostname;
+            var somethingWrongWithHostnameMessage;
+            var storage;
+            var suggestOperaMessage;
+            var x;
+
+            somethingWrongWithHostnameMessage = 'Something wrong with window.hostname: ';
+            suggestOperaMessage = 'Localhost is fine on Opera, but it lacks localStorage in Firefox, etc.';
+            try {
+                storage = window.localStorage;
+                if (null === storage) {
+                    hostname = window.location.hostname;
+                    if (null === hostname) {
+                        window.alert(somethingWrongWithHostnameMessage + hostname);
+                        return false;
+                    }
+                    if ('localhost' == hostname) {
+                        window.alert(suggestOperaMessage);
+                        return false;
+                    }
+                    return false;
+                }
+                x = '__storage_test__';
+                storage.setItem(x, x);
+                storage.removeItem(x);
+                return true;
+            }
+            catch(e) {
+                //First, do the popup.
+                window.alert('Remembering songs (across sessions) is disabled:  ' + e);
+
+                //Avoid QuotaExceededError, but only if the exception is a DOMException...
+                return e instanceof DOMException &&
+
+                    //and storage exists...
+                    null !== storage &&
+
+                    //and storage isn't undefined...
+                    undefined !== storage &&
+
+                    //and there's something already stored...
+                    storage.length !== 0 && (
+
+                        //and the exception is one of certain, known ones:
+                        //Because the name field might not be set, first check the error code...
+                        //for Firefox:
+                        //Legacy constant name: NS_ERROR_DOM_QUOTA_REACHED.
+                        e.code === 1014 ||
+
+                        //for everything except Firefox:
+                        //Legacy constant name: QUOTA_EXCEEDED_ERR.
+                        e.code === 22 ||
+
+                        //Because the error code might not be set, also check the name field...
+                        //for Firefox:
+                        e.name === 'NS_ERROR_DOM_QUOTA_REACHED' ||
+
+                        //for everything except Firefox:
+                        e.name === 'QuotaExceededError'
+                    );
+            }
+        }
 
         node = document.getElementById('main');
         app = Elm.Main.embed(node, {
@@ -97,70 +161,6 @@ functionResetSongsDevelopmentOnly();
                 window.localStorage.setItem(keyStorage, JSON.stringify(songsRememberedFromPort));
             }
         });
-    }
-    functionStorageIsAccessible = function() {
-        //See: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
-        var hostname;
-        var somethingWrongWithHostnameMessage;
-        var storage;
-        var suggestOperaMessage;
-        var x;
-
-        somethingWrongWithHostnameMessage = 'Something wrong with window.hostname: ';
-        suggestOperaMessage = 'Localhost is fine on Opera, but it lacks localStorage in Firefox, etc.';
-        try {
-            storage = window.localStorage;
-            if (null === storage) {
-                hostname = window.location.hostname;
-                if (null === hostname) {
-                    window.alert(somethingWrongWithHostnameMessage + hostname);
-                    return false;
-                }
-                if ('localhost' == hostname) {
-                    window.alert(suggestOperaMessage);
-                    return false;
-                }
-                return false;
-            }
-            x = '__storage_test__';
-            storage.setItem(x, x);
-            storage.removeItem(x);
-            return true;
-        }
-        catch(e) {
-            //First, do the popup.
-            window.alert('Remembering songs (across sessions) is disabled:  ' + e);
-
-            //Avoid QuotaExceededError, but only if the exception is a DOMException...
-            return e instanceof DOMException &&
-
-                //and storage exists...
-                null !== storage &&
-
-                //and storage isn't undefined...
-                undefined !== storage &&
-
-                //and there's something already stored...
-                storage.length !== 0 && (
-
-                    //and the exception is one of certain, known ones:
-                    //Because the name field might not be set, first check the error code...
-                    //for Firefox:
-                    //Legacy constant name: NS_ERROR_DOM_QUOTA_REACHED.
-                    e.code === 1014 ||
-
-                    //for everything except Firefox:
-                    //Legacy constant name: QUOTA_EXCEEDED_ERR.
-                    e.code === 22 ||
-
-                    //Because the error code might not be set, also check the name field...
-                    //for Firefox:
-                    e.name === 'NS_ERROR_DOM_QUOTA_REACHED' ||
-
-                    //for everything except Firefox:
-                    e.name === 'QuotaExceededError'
-                );
-        }
     }
 
     functionDealWithElm();
