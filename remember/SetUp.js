a8ddf84a936d73c800977ae332a8177081da1bc8
Mark D. Blackwell <markdblackwell01@gmail.com>
2017-10-19 10:39:47 -0400

    Refactor

--- remember/SetUp.js
+++ remember/SetUp.js
@@ -161,12 +161,12 @@ functionResetSongsDevelopmentOnly();
         });
 
         functionStorageSubscribe = function() {
-        //Don't use an arrow function ("fat tag"), because IE 11 doesn't support it.
-        app.ports.updateLocalStorage.subscribe(function(songsRememberedFromPort) {
-            if (functionStorageIsAccessible()) {
-                window.localStorage.setItem(keyStorage, JSON.stringify(songsRememberedFromPort));
-            }
-        });
+            //Don't use an arrow function ("fat tag"), because IE 11 doesn't support it.
+            app.ports.updateLocalStorage.subscribe(function(songsRememberedFromPort) {
+                if (functionStorageIsAccessible()) {
+                    window.localStorage.setItem(keyStorage, JSON.stringify(songsRememberedFromPort));
+                }
+            });
         }
 
         functionStorageSubscribe();
