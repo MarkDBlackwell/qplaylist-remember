0bcea93a9dcd3d8eead14417bc647856cb146093
Mark D. Blackwell <markdblackwell01@gmail.com>
2018-09-13 08:07:08 -0400

    Improve formatting

--- try/hello.php
+++ try/hello.php
@@ -1,10 +1,17 @@
-<html>
+<!DOCTYPE html>
+<!-- Copyright (C) 2018 Mark D. Blackwell.
+	All rights reserved.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+-->
+<html lang="en">
 <!-- from http://php.net/manual/en/tutorial.firstpage.php -->
- <head>
-  <meta charset="UTF-8">
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?>
- </body>
+	<head>
+		<meta charset="UTF-8">
+		<title>PHP Test</title>
+	</head>
+	<body>
+		<?php echo '<p>Hello World</p>'; ?>
+	</body>
 </html>
